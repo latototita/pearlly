1095eec1a5fb24f07b77388cf691117b18c4d1da
latototita <85679433+latototita@users.noreply.github.com>
2022-02-02 08:20:15 -0500

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 
-ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
